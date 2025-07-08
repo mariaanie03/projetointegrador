975f247e2dfd790a6247fdd9a850720d1d5e0ca4
mariaanie03 <santis.mariaanie@gmail.com>
2025-07-08 13:37:01 -0300

    Add files via upload

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,7 +2,7 @@
 //import { createClient } from '@supabase/supabase-js';
 
 // Suas credenciais Supabase (substitua pelos seus valores reais)
-const supabaseUrl = 'https://crlcdyiuyqgkyeuiahgb.supabase.co'; // Ex: 'https://abcdefghijk.supabase.co'
+const supabaseUrl = 'https://crlcdyiuyqgkyeuiahgb.supabase.co';// Ex: 'https://abcdefghijk.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNybGNkeWl1eXFna3lldWlhaGdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAxNzgyNzUsImV4cCI6MjA2NTc1NDI3NX0.y_rIdqY6ducucO0lTX4KjbxdJsD10V4BImKTKizk6O4'; // Ex: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'
 
 // Inicializa o cliente Supabase
@@ -14,6 +14,5 @@ const supabase = window.myCreateSupabaseClient(supabaseUrl, supabaseAnonKey);
 //export { supabase };
 
 console.log('Cliente Supabase inicializado com sucesso!');
-
 
 
